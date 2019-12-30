4d8554360d40f6299a1dbe413474feed5be3a951
Jacob Craig <jakecraig9@gmail.com>
2019-12-30 00:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function logWhisper (string) {
 function sayHiToGrandma (string) {
   return "I can\'t hear you!" ;
 }
-var uppercase = "HELLO!" ;
 function sayHiToGrandma (string) {
   return "YES INDEED!" ;
 }
